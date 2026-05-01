a7e7fa51aef2005b4abc537403f62e500e95a5a1
Antoni Seba <antoniwoj@o2.pl>
2026-05-01 14:56:59 +0200

    oje

--- scratch/test-build.ts
+++
@@ -1,6 +0,0 @@
-
-import { buildPaywallCopy } from './lib/admin/translation-source'
-import { COPY, type Copy } from './lib/paywall-copy'
-
-const testCopy = buildPaywallCopy({})
-console.log('Build successful')
